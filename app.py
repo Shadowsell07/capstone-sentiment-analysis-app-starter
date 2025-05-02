05907b0a244689a751adb3834a23e2b0b99622b4
Shadowsell07 <Dan.Sellers07@gmail.com>
2025-05-02 18:20:49 +0000

    latest changes

--- app.py
+++ app.py
@@ -1,8 +1,36 @@
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import sequence
 from flask import Flask, render_template, request
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
 analyzer = SentimentIntensityAnalyzer()
+
+# Initialize global variables
+model = None
+tokenizer = None
+
+# Define functions to load model and tokenizer
+def load_keras_model():
+    global model
+    model = load_model('models/uci_sentimentanalysis.h5')
+
+def load_tokenizer():
+    global tokenizer
+    with open('models/tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+# Define sentiment analysis function
+def sentiment_analysis(input):
+    user_sequences = tokenizer.texts_to_sequences([input])
+    user_sequences_matrix = sequence.pad_sequences(user_sequences, maxlen=1225)
+    prediction = model.predict(user_sequences_matrix)
+    return round(float(prediction[0][0]), 2)
+
+# Initialize model and tokenizer at startup
+load_keras_model()
+load_tokenizer()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -10,7 +38,8 @@ def index():
     if request.method == "POST":
         text = request.form.get("user_text")
         sentiment = analyzer.polarity_scores(text)
+        sentiment["custom model positive"] = sentiment_analysis(text)
     return render_template('form.html', sentiment=sentiment)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
